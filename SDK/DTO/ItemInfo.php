dcb05ddfb7837e1ff9e55b49ff7e1a7fe66e0b6e
Alexandr Dmitriev <admitriev@oroinc.com>
2018-03-07 18:56:09 +0200

    BB-13886: Create SDK anti-corruption layer

---
+++ SDK/DTO/ItemInfo.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Oro\Bundle\PayPalExpressBundle\SDK\DTO;
+
+class ItemInfo
+{
+    /**
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * @var string
+     */
+    protected $currency;
+
+    /**
+     * @var int
+     */
+    protected $quantity;
+
+    /**
+     * @var float
+     */
+    protected $price;
+
+    /**
+     * ItemInfo constructor.
+     * @param string $name
+     * @param string $currency
+     * @param int    $quantity
+     * @param float  $price
+     */
+    public function __construct(string $name, string $currency, int $quantity, float $price)
+    {
+        $this->name     = $name;
+        $this->currency = $currency;
+        $this->quantity = $quantity;
+        $this->price    = $price;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCurrency()
+    {
+        return $this->currency;
+    }
+
+    /**
+     * @return int
+     */
+    public function getQuantity()
+    {
+        return $this->quantity;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+}
---
+++ SDK/DTO/PaymentInfo.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace Oro\Bundle\PayPalExpressBundle\SDK\DTO;
+
+class PaymentInfo
+{
+    const PAYMENT_METHOD_PAYPAL = 'paypal';
+
+    /**
+     * @var float
+     */
+    protected $totalAmount;
+
+    /**
+     * @var string
+     */
+    protected $description;
+
+    /**
+     * @var string
+     */
+    protected $currency;
+
+    /**
+     * @var float
+     */
+    protected $shipping;
+
+    /**
+     * @var float
+     */
+    protected $tax;
+
+    /**
+     * @var float
+     */
+    protected $subtotal;
+
+    /**
+     * @var string
+     */
+    protected $method;
+
+    /**
+     * @var ItemInfo[]
+     */
+    protected $items = [];
+
+    /**
+     * @param float      $totalAmount
+     * @param string     $description
+     * @param string     $currency
+     * @param float      $shipping
+     * @param float      $tax
+     * @param float      $subtotal
+     * @param string     $method
+     * @param ItemInfo[] $items
+     */
+    public function __construct(
+        $totalAmount,
+        $description,
+        $currency,
+        $shipping,
+        $tax,
+        $subtotal,
+        $method,
+        array $items
+    ) {
+        $this->totalAmount = $totalAmount;
+        $this->description = $description;
+        $this->currency    = $currency;
+        $this->shipping    = $shipping;
+        $this->tax         = $tax;
+        $this->subtotal    = $subtotal;
+        $this->items       = $items;
+    }
+
+    /**
+     * @return float
+     */
+    public function getTotalAmount()
+    {
+        return $this->totalAmount;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCurrency()
+    {
+        return $this->currency;
+    }
+
+    /**
+     * @return float
+     */
+    public function getShipping()
+    {
+        return $this->shipping;
+    }
+
+    /**
+     * @return float
+     */
+    public function getTax()
+    {
+        return $this->tax;
+    }
+
+    /**
+     * @return float
+     */
+    public function getSubtotal()
+    {
+        return $this->subtotal;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMethod()
+    {
+        return $this->method;
+    }
+
+
+    /**
+     * @return ItemInfo[]
+     */
+    public function getItems()
+    {
+        return $this->items;
+    }
+}
