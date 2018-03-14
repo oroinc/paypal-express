80e7c6b5da833fcbefd3e1a84272845d0b161ab6
Alexandr Dmitriev <admitriev@oroinc.com>
2018-03-14 16:11:03 +0200

    BB-13886: Create SDK anti-corruption layer - add method translator test

---
+++ Tests/Unit/Method/Translator/MethodConfigTranslatorTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Oro\Bundle\PayPalExpressBundle\Tests\Unit\Method\Translator;
+
+use Oro\Bundle\PayPalExpressBundle\Method\Config\PayPalExpressConfig;
+use Oro\Bundle\PayPalExpressBundle\Method\Translator\MethodConfigTranslator;
+use Oro\Bundle\PayPalExpressBundle\Transport\DTO\ApiContextInfo;
+use Oro\Bundle\PayPalExpressBundle\Transport\DTO\CredentialsInfo;
+
+class MethodConfigTranslatorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetApiContextInfo()
+    {
+        $translator = new MethodConfigTranslator();
+
+        $clientId = 'AxBU5pnHF6qNArI7Nt5yNqy4EgGWAU3K1w0eN6q77GZhNtu5cotSRWwZ';
+        $clientSecret = 'BxBU5pnHF6qNArI7Nt5yNqy4EgGWAU3K1w0eN6q77GZhNtu5cotSRWwZ';
+        $isSandbox = false;
+
+        $expectedApiContextInfo = new ApiContextInfo(
+            new CredentialsInfo(
+                $clientId,
+                $clientSecret
+            ),
+            $isSandbox
+        );
+
+        $config = new PayPalExpressConfig(
+            'test',
+            'test',
+            'test',
+            $clientId,
+            $clientSecret,
+            $isSandbox
+        );
+
+        $apiContextInfo = $translator->getApiContextInfo($config);
+        $this->assertEquals($expectedApiContextInfo, $apiContextInfo);
+    }
+}
