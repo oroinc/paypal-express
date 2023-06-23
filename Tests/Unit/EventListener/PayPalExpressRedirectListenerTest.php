cfb31c12202eee0ffe7206ec74bc4d28e3551e3d
Vova Soroka <vsoroka@oroinc.com>
2023-06-23 10:55:01 +0300

    BAP-22125: Define visibility of constants in tests (#35917)

--- Tests/Unit/EventListener/PayPalExpressRedirectListenerTest.php
+++ Tests/Unit/EventListener/PayPalExpressRedirectListenerTest.php
@@ -21,7 +21,7 @@ use Symfony\Component\HttpFoundation\Session\Session;
  */
 class PayPalExpressRedirectListenerTest extends \PHPUnit\Framework\TestCase
 {
-    /** @var PaymentMethodProviderInterface|\PHPUnit\Framework\MockObject\MockObject  */
+    /** @var PaymentMethodProviderInterface|\PHPUnit\Framework\MockObject\MockObject */
     private $paymentMethodProvider;
 
     /** @var PaymentResultMessageProviderInterface|\PHPUnit\Framework\MockObject\MockObject */
